c2df77d06869a7ad8febba21dd9f548d8d9f4c0b
corvinus-d8x2f4 <zalan.lengyel@stud.uni-corvinus.hu>
2022-10-07 11:19:50 +0200

    tábla létrehozása

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -72,7 +72,13 @@ namespace WindowsFormsApp1
             foreach (Flat f in Flats)
             {
                 values[counter, 0] = f.Code;
-                // ...
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
+                values[counter, 4] = f.Elevator;
+                values[counter, 5] = f.NumberOfRooms;
+                values[counter, 6] = f.FloorArea;
+                values[counter, 7] = f.Price;
                 values[counter, 8] = "";
                 counter++;
             }
