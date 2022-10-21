60371d8d526bd932df9abad59b779a558436c322
corvinus-d8x2f4 <zalan.lengyel@stud.uni-corvinus.hu>
2022-10-21 10:41:59 +0200

    xml

--- 5_gyak/5_gyak/Form1.cs
+++ 5_gyak/5_gyak/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace _5_gyak
 {
@@ -22,10 +23,37 @@ namespace _5_gyak
             InitializeComponent();
             dataGridView1.DataSource = Rates;
             GetExchangeRates();
-
+            GetXml();
         }
 
-        private void GetExchangeRates()
+        public void GetXml()
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                // Létrehozzuk az adatsort és rögtön hozzáadjuk a listához
+                // Mivel ez egy referencia típusú változó, megtehetjük, hogy előbb adjuk a listához és csak később töltjük fel a tulajdonságait
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                // Dátum
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                // Valuta
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                // Érték
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
+        }
+
+        public void GetExchangeRates()
         {
             var mnbService = new MNBArfolyamServiceSoapClient();
             var request = new GetExchangeRatesRequestBody()
